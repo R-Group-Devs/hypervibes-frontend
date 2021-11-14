89025755ea4e4c513b8ac15bc8feb5fb436cb05a
Brandon Valosek <bvalosek@gmail.com>
2021-11-14 15:24:27 -0600

    implement browse realm, browse realm nfts

--- src/pages/BrowseNftsPage.tsx
+++ src/pages/BrowseNftsPage.tsx
@@ -1,5 +1,7 @@
 import { useParams } from 'react-router';
 import styled from 'styled-components';
+import { NETWORKS } from '../constants/contracts';
+import useListRealmNfts from '../hooks/useListRealmNfts';
 
 interface Params {
   network: string;
@@ -10,11 +12,41 @@ const Container = styled.div``;
 
 export default () => {
   const { network, realmId } = useParams<Params>();
+  const { data, isLoading, isError } = useListRealmNfts(
+    realmId,
+    NETWORKS[network]
+  );
+
+  const chainId = NETWORKS[network];
+  if (chainId == null) {
+    return <p>invalid network</p>;
+  }
+
+  if (isLoading) {
+    return <p>loading realm nfts...</p>;
+  }
+
+  if (isError) {
+    return <p>error fetching nfts</p>;
+  }
+
+  if (data == null) {
+    return null;
+  }
+
   return (
     <Container>
-      <p>
+      <h1>
         browse nfts - {network}, realmId={realmId}
-      </p>
+      </h1>
+      <div>
+        {data.realm.infusions.map(infusion => (
+          <div key={infusion.id}>
+            {infusion.nft.collection.name} ({infusion.nft.collection.symbol}),
+            tokenId={infusion.nft.tokenId}
+          </div>
+        ))}
+      </div>
     </Container>
   );
 };
--- src/pages/BrowseRealmsPage.tsx
+++ src/pages/BrowseRealmsPage.tsx
@@ -1,5 +1,8 @@
 import { useParams } from 'react-router';
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
+import { NETWORKS } from '../constants/contracts';
+import useListRealms from '../hooks/useListRealms';
 
 interface Params {
   network: string;
@@ -7,11 +10,50 @@ interface Params {
 
 const Container = styled.div``;
 
+const RealmRow = styled.div``;
+
 export default () => {
   const { network } = useParams<Params>();
+  const { data, isLoading, isError } = useListRealms(NETWORKS[network]);
+
+  const chainId = NETWORKS[network];
+  if (chainId == null) {
+    return <p>invalid network</p>;
+  }
+
+  if (isError) {
+    return <p>error fetching realms</p>;
+  }
+
+  if (isLoading) {
+    return <p>loading realms...</p>;
+  }
+
+  if (data == null) {
+    return null;
+  }
+
   return (
     <Container>
-      <p>browse realms - {network}</p>
+      <h1>browse realms - {network}</h1>
+      <div>
+        {data.realms.map(realm => (
+          <RealmRow key={realm.id}>
+            <div>
+              {realm.id}:
+              <Link to={`/${network}/realms/${realm.id}/nfts`}>
+                {realm.name}
+              </Link>
+            </div>
+            <div>
+              description: {realm.description} <br />
+              created: {realm.createdAtTimestamp}
+              <br />
+              token: {realm.token.name} ({realm.token.symbol})<br />
+            </div>
+          </RealmRow>
+        ))}
+      </div>
     </Container>
   );
 };
