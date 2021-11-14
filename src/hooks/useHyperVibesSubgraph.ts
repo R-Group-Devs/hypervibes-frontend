7db20adc50a71a512959092995c40604cd11b81e
Brandon Valosek <bvalosek@gmail.com>
2021-11-14 14:16:00 -0600

    tweak contract addresses, allow explicit chain id for subgraph

--- src/hooks/useHyperVibesSubgraph.ts
+++ src/hooks/useHyperVibesSubgraph.ts
@@ -1,13 +1,16 @@
 import { useQuery, UseQueryOptions } from 'react-query';
 import { request } from 'graphql-request';
 import { useWallet } from 'use-wallet';
+import { NETWORKS } from '../constants/contracts';
 
 const HYPERVIBES_SUBGRAPH_ENDPOINTS: Record<string, string> = {
-  // TODO: add mainnet URL
-  1: 'https://api.thegraph.com/subgraphs/name/r-group-devs/hypervibes-rinkeby',
-  3: 'https://api.thegraph.com/subgraphs/name/r-group-devs/hypervibes-ropsten',
-  4: 'https://api.thegraph.com/subgraphs/name/r-group-devs/hypervibes-rinkeby',
-  80001:
+  [NETWORKS.eth]:
+    'https://api.thegraph.com/subgraphs/name/r-group-devs/hypervibes',
+  [NETWORKS.ropsten]:
+    'https://api.thegraph.com/subgraphs/name/r-group-devs/hypervibes-ropsten',
+  [NETWORKS.rinkeby]:
+    'https://api.thegraph.com/subgraphs/name/r-group-devs/hypervibes-rinkeby',
+  [NETWORKS.mumbai]:
     'https://api.thegraph.com/subgraphs/name/r-group-devs/hypervibes-mumbai',
 };
 
@@ -17,14 +20,15 @@ export default <QueryResult>(
   queryOptions?: Omit<
     UseQueryOptions<QueryResult, Error>,
     'queryKey' | 'queryFn'
-  >
+  > & { chainId?: number }
 ) => {
-  // TODO: don't default to chain, instead make query condiitonal on chainId
-  // handle cases where no wallet is connected
-  const { chainId = 1 } = useWallet();
+  const { chainId: connectedChainId } = useWallet();
+  const chainId = queryOptions?.chainId ?? connectedChainId ?? 1;
 
   return useQuery<QueryResult, Error>(
-    queryName,
+    // including chain id in the cache key to ensure we don't mix cached queries
+    // across chains
+    `${chainId}:${queryName}`,
     async () => request(HYPERVIBES_SUBGRAPH_ENDPOINTS[chainId], query),
     queryOptions
   );
