7a7968fc92e5d9463bf09148b8d0f847e8388f71
Brandon Valosek <bvalosek@gmail.com>
2021-11-14 23:00:23 -0600

    update ropsten address

--- src/constants/contracts.ts
+++ src/constants/contracts.ts
@@ -7,7 +7,7 @@ export const NETWORKS: Record<string, number> = {
 
 export const HYPERVIBES_CONTRACT_ADDRESSES: Record<string, string> = {
   [NETWORKS.eth]: '0x0',
-  [NETWORKS.ropsten]: '0xcd181fB818aaAae8D34D6D5bBe7aD4c44ac8af98',
+  [NETWORKS.ropsten]: '0x91367F9C5a07912a4870ceA43b3893366c05b35c',
   [NETWORKS.rinkeby]: '0xafb96b99a0A4eF348115C6BbD99A71d3d4F52Ff1',
   [NETWORKS.mumbai]: '0x57FBF9E899E17E23d46425e33eE191C8FaD27c28',
 };
